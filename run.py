fcc4825d66b1b864092aff3b2bb3de40f3b387f0
Esmond <esmondpeh@gmail.com>
2015-11-04 22:12:13 +0800

    step 1: run kismet done

--- run.py
+++ run.py
@@ -1,12 +1,110 @@
 #!/usr/bin/python
-import re 	# for Regex library
 
+import re
+import os
 
 '''
 Step 1: Run Kismet 
-Step 2: Rename pcapbtbb file name
+'''
+import subprocess
+import sys
+
+run_kismet = None
+
+try: 
+    if (subprocess.call(['kismet', '-version']) == 0):
+    	run_kismet = True
+    	print "Found"
+except: 
+    print "No Kismet executable found"
+    sys.exit(0)
+
+'''
+Step 2: After running Kismet for 15 min, rename pcapbtbb file name
+'''
+'''
+import threading
+import os
+import shutil
+
+def renameFile():
+    print "Renaming pcapbtbb file ..."
+    if run_kismet:
+	for filename in os.listdir("."):
+		# find file name that starts with Kismet
+		if filename.startswith("Kismet"):
+			# Copy file to another file
+			shutil.copy2(filename, 'bluecount.pcapbtbb')
+
+			# Remove original file
+			os.remove(filename)
+
+
+# Timmer(seconds, call)
+threading.Timer(900.0, renameFile).start()
+
+
+
+def printit():
+  threading.Timer(5.0, printit).start()
+  print "Hello, World!"
+
+#printit()
+'''
+
+'''
+
+
+if run_kismet:
+	for filename in os.listdir("."):
+		# find file name that starts with Kismet
+		if filename.startswith("Kismet"):
+			# Copy file to another file
+			shutil.copy2(filename, 'bluecount.pcapbtbb')
+
+			# Remove original file
+			os.remove(filename)
+'''
+'''
 Step 3: Run Wireshark
+'''
+
+'''
 Step 4: Read txt file
+'''
+# Regex pattern for matching string in PCAP
+# An example string in file is 741 572.433708 00:00:00_b8:89:84 -> 00:00:00_00:00:00 0xfff0 14 Ethernet II
+# pattern only reads up to 741 572.433708 00:00:00_b8:89:84
+pattern = re.compile("\s*\d{1,}\s*\d{1,}[.]\d{1,}\s*([0-9a-fA-F][0-9a-fA-F]:){2}[0-9a-fA-F][0-9a-fA-F][_](([0-9a-fA-F][0-9a-fA-F]:){2}([0-9a-fA-F][0-9a-fA-F]))")
+
+# Using a dict to store LAP with keys as LAP so there will be no duplicates
+mac_dict = {}
+
+def printLapAddress():
+	if len(mac_dict) > 0:
+		for keys,values in mac_dict.items():
+			print(keys)
+
+def printLapCount():
+	print len(mac_dict)
+
+def readFile():
+	# Open txt file for reading only in binary format. The file pointer is placed at the beginning of the file. This is the default mode.
+    with open("openwithtshark.txt", "rb") as pcap_file:
+        for line in pcap_file:
+
+    	    # Get a match from each line with the pattern
+    	    match_found = pattern.match(line)
+
+    	    if match_found:
+    	    	# Store LAP entry to dict using LAP as key
+    	    	mac_dict[match_found.group(2)] = match_found.group(2)
+
+#readFile()
+#printLapCount()
+#printLapAddress()
+
+
 '''
 
 # Regex pattern for matching string in PCAP
@@ -71,3 +169,4 @@ def step3():
 	output, err = prog_kismet.communicate()
 	print "*** Running Kismet ***\n", output
 
+'''
