a699d9afda4d9901a000a692824373c90131119b
Esmond <esmondpeh@gmail.com>
2015-11-05 15:48:51 +0800

    fix bugs in using tshark command

--- run.py
+++ run.py
@@ -1,77 +1,12 @@
 #!/usr/bin/python
 
 import re
-import os
-
-'''
-Step 1: Run Kismet 
-'''
-import subprocess
-import sys
-
-try: 
-    subprocess.call(['kismet'])
-except: 
-    print "No Kismet executable found"
-    sys.exit(0)
-
-
-'''
-Step 2: After running Kismet for 15 min, rename pcapbtbb file name. 
-
-Read pcapbtbb file using wireshark and pipe to a text file.
-This will repeat for every other 15 min when Kismet is still running
-'''
 import threading
 import os
 import shutil
+import subprocess
+import sys
 
-def pipePcaptoText():
-	try:
-		# read pcapbtbb file using wireshark and pipe to text file
-		subprocess.call(['tshark','r','bluecount.pcapbtbb','>','bluecount.txt'])
-		
-		print "Piped to text file, remove pcapbtbb file"
-		
-		# delete pcapbtbb after pipe
-		if filename.startswith("bluecount"):
-			os.remove(filename)
-
-		# read file
-		readFile()
-
-		# print LAP count
-		printLapCount()
-
-		# print LAP addresses found
-		printLapAddress()
-
-	except:
-		print "Invalid command"
-		sys.exit(0)
-
-def renameFile():
-	# set timer to repeat every 15 minutes
-	threading.Timer(900.0, renameFile).start()
-	print "After 15 min, renaming pcapbtbb file ..."
-	for filename in os.listdir("."):
-		# find file name that starts with Kismet
-		if filename.startswith("Kismet"):
-			# Copy file to another file
-			shutil.copy2(filename, 'bluecount.pcapbtbb')
-			# Remove original file
-			os.remove(filename)
-			# open pcapbtbb file using wireshark and pipe to text file
-			# delete pcapbtbb after pipe
-			pipePcaptoText()
-
-
-# Timer(seconds, call)
-threading.Timer(900.0, renameFile).start()
-
-'''
-Step 3: Read txt file and output total cost of unique LAP captured
-'''
 # Regex pattern for matching string in PCAP
 # An example string in file is 741 572.433708 00:00:00_b8:89:84 -> 00:00:00_00:00:00 0xfff0 14 Ethernet II
 # pattern only reads up to 741 572.433708 00:00:00_b8:89:84
@@ -81,24 +16,84 @@ pattern = re.compile("\s*\d{1,}\s*\d{1,}[.]\d{1,}\s*([0-9a-fA-F][0-9a-fA-F]:){2}
 mac_dict = {}
 
 def printLapAddress():
+	print "\nPrinting LAP Address\n===================="
 	if len(mac_dict) > 0:
 		for keys,values in mac_dict.items():
 			print(keys)
 
 def printLapCount():
-	print len(mac_dict)
+	print "\nPrinting LAP count\n=================="
+	print "Total count :", len(mac_dict)
 
 def readFile():
+	print "\nReading text file ...\n====================="
 	# Open txt file for reading only in binary format. The file pointer is placed at the beginning of the file. This is the default mode.
-    with open("bluecount.txt", "rb") as pcap_file:
-        for line in pcap_file:
+	with open("bluecount.txt", "rb") as pcap_file:
+		for line in pcap_file:
+			# Get a match from each line with the pattern
+			match_found = pattern.match(line)
 
-    	    # Get a match from each line with the pattern
-    	    match_found = pattern.match(line)
+			if match_found:
+				# Store LAP entry to dict using LAP as key
+				mac_dict[match_found.group(2)] = match_found.group(2)
+	# close file after read to free up any system resources taken up by the open file
+	pcap_file.close()
+	print "\n>>> DONE"
 
-    	    if match_found:
-    	    	# Store LAP entry to dict using LAP as key
-    	    	mac_dict[match_found.group(2)] = match_found.group(2)
+def pipePcaptoText(pcapfile, textfile):
+	try:
+		print "\nUse tshark to read", pcapfile, "\n====================================="
+		# read pcapbtbb file using wireshark and save to out
+		out = subprocess.check_output(['tshark','-r', pcapfile])
 
-    # close file after read to free up any system resources taken up by the open file
-    pcap_file.close()
+		if out:
+			print "\nSave output to", textfile
+
+			# save out to text file
+			with open(textfile, "w") as text_file:
+				text_file.write(out)
+			text_file.close()
+			print "\n>>> DONE"
+
+	except:
+		print "ERROR: Invalid command"
+		sys.exit(0)
+
+def makeCopyOfPcapFile(new_filename):
+	# set timer to repeat every 15 minutes
+	#threading.Timer(900.0, renameFile).start()
+
+	for filename in os.listdir("."):
+		# find file name that starts with Kismet
+		if filename.startswith("Kismet") and filename.endswith(".pcapbtbb"):
+			print "\nMaking a copy of", filename, "to file", new_filename, "\n==============================================================================="
+			# Copy file to another file
+			shutil.copy2(filename, new_filename)
+			
+	print "\n>>> DONE"
+
+def cleanup():
+	print "\nRevert all operations on pcapbtbb file\n======================================"
+	for filename in os.listdir("."):
+		# find file name
+		if filename == "bluecount.txt":
+			os.remove(filename)
+			print "Removed text file"
+		if filename == "bluecount.pcapbtbb":
+			os.remove(filename)
+			print "Removed bluecount.pcapbtbb file"
+	print "\n>>> DONE"
+
+def run():
+	# Set timer
+	print "\nRun this in every 15 seconds"
+	threading.Timer(15.0, run).start()
+
+	makeCopyOfPcapFile("bluecount.pcapbtbb")
+	pipePcaptoText("bluecount.pcapbtbb", "bluecount.txt")
+	readFile()
+	printLapAddress()
+	printLapCount()
+	cleanup()
+
+run()
